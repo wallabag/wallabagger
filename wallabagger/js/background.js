70e09373d91f18577446850ff13ffc021006feea
Simounet <contact@simounet.net>
2016-12-05 13:46:41 +0100

    URL facto + light linting

--- wallabagger/js/background.js
+++ wallabagger/js/background.js
@@ -13,33 +13,28 @@ browser.contextMenus.onClicked.addListener(function(info) {
     switch (info.menuItemId) {
         case "wallabagger-add-link":
             browser.browserAction.setIcon({path: 'img/wallabagger-yellow.svg'});
-            this.api =  new WallabagApi();
-              let apiAuthorised = this.api.load()
-                     .then(data =>{
-                        if ( this.api.needNewAppToken() ){
-                            return this.api.GetAppToken();
-                        }
-                        return 'OK'               
-                    })
-                    .catch(error=>{
-                            throw error;    
-                   });
-            var url = "";
-
-            if (info.linkUrl !== undefined )
-              url = info.linkUrl;
-            else 
-              url = info.pageUrl;
-
-            apiAuthorised.then(() => this.api.SavePage(url) ) 
-                         .then(r =>  { 
-                             browser.browserAction.setIcon({path: 'img/wallabagger-green.svg'}); 
-                             setTimeout(function() { browser.browserAction.setIcon({path: browserActionIconDefault}); }, 5000); 
-                             }) 
-                         .catch( e => { 
-                              browser.browserAction.setIcon({path: 'img/wallabagger-red.svg'});
-                             setTimeout(function() { browser.browserAction.setIcon({path: browserActionIconDefault}); }, 5000); 
-                            }); 
+            this.api = new WallabagApi();
+            const apiAuthorised = this.api.load()
+                   .then(data => {
+                      if ( this.api.needNewAppToken() ) {
+                          return this.api.GetAppToken();
+                      }
+                      return true;
+                  })
+                  .catch(error => {
+                          throw error;
+                 });
+            const url = typeof( info.linkUrl ) === 'string' ? info.linkUrl : info.pageUrl;
+            apiAuthorised
+                .then(() => this.api.SavePage(url) )
+                .then(r => {
+                    browser.browserAction.setIcon({path: 'img/wallabagger-green.svg'});
+                    setTimeout(function() { browser.browserAction.setIcon({path: browserActionIconDefault}); }, 5000);
+                })
+                .catch( e => {
+                    browser.browserAction.setIcon({path: 'img/wallabagger-red.svg'});
+                    setTimeout(function() { browser.browserAction.setIcon({path: browserActionIconDefault}); }, 5000);
+                });
 
             break;
     }
