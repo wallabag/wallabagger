1ad2faf4c033e3cb75900fc14e4cf5a98f31abb9
Adrien Gallou <adriengallou@gmail.com>
2021-12-05 16:36:42 +0100

    adds an option to conditionaly fetch locally the page data
    Some websites like https://www.arretsurimages.net are Single Page Applications behind a Paywall.
    Those websites cannot be fetched by wallabag using the credentials API.
    
    This commits adds an option where you can list the websites that you want to fetch locally.

--- wallabagger/js/background.js
+++ wallabagger/js/background.js
@@ -213,7 +213,7 @@ function onPortMessage (msg) {
     try {
         switch (msg.request) {
             case 'save':
-                savePageToWallabag(msg.tabUrl, false);
+                savePageToWallabag(msg.tabUrl, false, msg.title, msg.content);
                 break;
             case 'tags':
                 if (!cache.check('allTags')) {
@@ -463,7 +463,7 @@ function moveToDirtyCache (url) {
     }
 }
 
-function savePageToWallabag (url, resetIcon) {
+function savePageToWallabag (url, resetIcon, title, content) {
     if (isServicePage(url)) {
         return;
     }
@@ -493,7 +493,18 @@ function savePageToWallabag (url, resetIcon) {
     browserIcon.set('wip');
     existCache.set(url, existStates.wip);
     postIfConnected({ response: 'info', text: Common.translate('Saving_the_page_to_wallabag') });
-    api.SavePage(url)
+
+    const savePageOptions = {
+        url: url
+    };
+
+    if (api.IsSiteToFetchLocally(url)) {
+        savePageOptions.title = title;
+        savePageOptions.content = content;
+    }
+
+    const promise = api.SavePage(savePageOptions);
+    promise
         .then(data => applyDirtyCacheLight(url, data))
         .then(data => {
             if (!data.deleted) {
