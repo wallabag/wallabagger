0e2eb51490c8957dc5ab34312439953e13b8dd72
Simounet <contact@simounet.net>
2025-06-30 13:05:38 +0200

    Dev traces removed

--- wallabagger/js/browser-polyfill.js
+++ wallabagger/js/browser-polyfill.js
@@ -3,13 +3,11 @@
 const browser = (() => {
     globalThis.isChromeBrowser = typeof (globalThis.browser) === 'undefined' && typeof (chrome) === 'object';
     if (globalThis.isChromeBrowser) {
-        console.log('Chrome');
         globalThis.wallabaggerBrowser = 'Chrome';
         globalThis.browser = chrome;
         return chrome;
     }
 
-    console.log('Firefox');
     globalThis.wallabaggerBrowser = 'Firefox';
     return globalThis.browser;
 })();
